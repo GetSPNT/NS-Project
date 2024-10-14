aefc5d950bf4686c8c2b4ed87ba7ea8c905dee7a
GetSPNT <getspnt@gmail.com>
2024-10-14 21:54:55 +0700

    start project

--- ns-angular/src/app/app.routes.ts
+++ ns-angular/src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', loadComponent: () => import('./auth/login/login.component').then(c => c.LoginComponent)},
-  {path: 'forgetpassword', loadComponent: () => import('./auth/forgetpassword/forgetpassword.component').then(c => c.ForgetpasswordComponent)},
-  {path: 'changepassword', loadComponent: () => import('./auth/changepassword/changepassword.component').then(c => c.ChangepasswordComponent)},
-  {path: 'setting', loadComponent: () => import('./view/setting/setting.component').then(c => c.SettingComponent)},
-  {path: 'createschduling', loadComponent: () => import('./view/create-schedule/create-schedule.component').then(c => c.CreateScheduleComponent)}
-];
+export const routes: Routes = [];
